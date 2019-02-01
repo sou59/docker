7a754e750d4a2dc2eee60cd3c923b1ab4d5ad09a
Sou59 <sz5962@gmail.com>
2019-02-01 16:12:54 +0100

    docker-compose

--- network/web/index.php
+++ network/web/index.php
@@ -1,8 +1,9 @@
 <?php
 
-$pdo = new PDO('mysql:mysql_server;dbname=test;port:3306;charset=utf8', 'root', 'root');
+// $pdo = new PDO('mysql:mysql_server;dbname=test;port:3306;charset=utf8', 'root', 'root');
 
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FECH_OBJ);
+// $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FECH_OBJ);
 
+phpinfo();
